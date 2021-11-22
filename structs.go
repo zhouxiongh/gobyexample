74f16a0a3eedf5d7894610c7f60e4690454d475a
Jason He <hezhoux@qq.com>
2021-11-23 00:30:44 +0800

    Update structs.go

--- structs.go
+++ structs.go
@@ -1,28 +1,30 @@
 package main
 
+import "fmt"
+
 type person struct {
 	name string
 	age  int
 }
 
-//func main() {
-//	fmt.Println(person{"Bob", 20})
-//	fmt.Println(person{name: "Alice", age: 30})
-//	fmt.Println(person{name: "Fred"})
-//
-//	fmt.Println(&person{"Ann", 40})
-//
-//	s := person{"Sean", 50}
-//	fmt.Println(s.name)
-//
-//	// value copy
-//	// sp := s
-//	// ref copy
-//	sp := &s
-//	fmt.Println(sp.age)
-//
-//	sp.age = 51
-//	fmt.Println(sp.age)
-//	fmt.Println(s.age)
-//
-//}
+func main() {
+	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name: "Alice", age: 30})
+	fmt.Println(person{name: "Fred"})
+
+	fmt.Println(&person{"Ann", 40})
+
+	s := person{"Sean", 50}
+	fmt.Println(s.name)
+
+	// value copy
+	// sp := s
+	// ref copy
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+	fmt.Println(s.age)
+
+}
